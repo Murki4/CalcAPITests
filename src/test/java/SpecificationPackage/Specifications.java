5c50ea8d4ce4d5342cb68db180101d481952525b
Murki4 <kirill_shishkovs@mail.ru>
2024-05-22 23:12:03 +0900

    GetOperator Tests + Post Tests tweaks + Specification proper use

--- src/test/java/SpecificationPackage/Specifications.java
+++ src/test/java/SpecificationPackage/Specifications.java
@@ -3,10 +3,9 @@ package SpecificationPackage;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
+
 
 public class Specifications{
     private static final String uri = "https://calc.sintetika.keenetic.pro/api/";
@@ -19,14 +18,8 @@ public class Specifications{
                 .build();
     }
 
-    public static ResponseSpecification responseSpec(int i){
-        return new ResponseSpecBuilder()
-                .expectStatusCode(i)
-                .build();
-    }
-    public static void Install(RequestSpecification request, ResponseSpecification response){
+    public static void Install(RequestSpecification request){
         RestAssured.requestSpecification = request;
-        RestAssured.responseSpecification = response;
     }
     public static RequestSpecification authCred(){
         return RestAssured.given().auth().preemptive().basic("sys0b1_5", "ymJ-q7z-7N2-SZz");
